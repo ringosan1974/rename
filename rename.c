ca54dea9076267bd41c15a4c5beabcf029dc610e
ringosan1974 <wacyakun@gmail.com>
2022-05-08 02:45:54 +0900

    クソコード大幅改良

--- rename.c
+++ rename.c
@@ -6,9 +6,10 @@
 #include <unistd.h>
 #define _POSIX_SOURSE
 
-int file_rename(const char* fn, const char* nfn);
-char* connect_str_int(const char* c1, int num);
-int align_scanf_s(char* cp, const char* c);
+int fileIsBeing(const char *fn);
+int rename_s(const char *fn, const char *newfn);
+char *connectStrInt(const char *p, int num);
+int ask(char *p, const char *c);
 
 int main(void){
   DIR* dir;
@@ -16,49 +17,60 @@ int main(void){
   struct stat stat_buf;
   char path[128]; //ディレクトリのパス
   char new_fn[32];
-  int fnum_count = 0;
-  align_scanf_s(path, "path");
-  align_scanf_s(new_fn, "new file name");
+  int count = 0;
+
+  ask(path, "path");
+  ask(new_fn, "new file name");
+
+  //ディレクトリの存在判定
   if(stat(path, &stat_buf) != 0){
-    printf("そのPathは存在しません\n");
-    exit(0);
+    printf("そのディレクトリは存在しません\n");
+    exit(1);
   }
+
   dir = opendir(path);
+
   chdir(path);  //このプログラムのカレントディレクトリ変更
-  while((ds = readdir(dir)) != NULL){ //すべてのfileを読み込む
+  while((ds = readdir(dir)) != NULL){
     if(ds->d_name[0] == '.'){ //隠しファイルはrename()しない
       continue;
     }
-    file_rename(ds->d_name, connect_str_int(new_fn, fnum_count));
-    fnum_count++;
+    rename_s(ds->d_name, connectStrInt(new_fn, count));
+    count++;
   }
+
   closedir(dir);
   return 0;
 }
 
-//ファイルにアクセスできるかどうかを確かめてrename
-int file_rename(const char* fn, const char* nfn){
-  if(access(nfn, F_OK) != 0){
-    if(rename(fn, nfn) != 0){
-      return 1;
-    }
-  }else{
-    return 0;
+//ファイルにアクセスできるか確かめる
+//存在しなければ強制終了
+int fileIsBeing(const char *fn) {
+  if(access(fn, F_OK) != 0){
+    printf("ファイルが存在しません\n");
+    exit(1);
   }
+  return 0;
 }
 
-//文字列と数値を連結
-char* connect_str_int(const char* c1, int num){
+int rename_s(const char *fn, const char *newfn){
+  fileIsBeing(newfn);
+  rename(fn, newfn);
+  return 0;
+ }
+
+//sprintfして文字列を返す
+char *connectStrInt(const char *p, int num){
   char* c;
-  sprintf(c, "%s%X", c1, num);
+  sprintf(c, "%s%X", p, num);
   return c;
 }
 
-int align_scanf_s(char* cp, const char* c){
+int ask(char *p, const char *c){
   printf("%s : ", c);
-  if(scanf("%s", cp) == EOF){
+  if(scanf("%s", p) == EOF){
     printf("error\n");
-    return 1;
+    exit(1);
   }else{
     return 0;
   }
